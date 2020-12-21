92f203a64cd9b47fbd41ef773d48a00f4be53909
torantie <michi.maeh@googlemail.com>
2020-12-21 11:30:14 +0100

    Initial Commit

--- homeworks/solutions/torantie/4-param/src/halfedge.js
+++ homeworks/solutions/torantie/4-param/src/halfedge.js
@@ -42,7 +42,22 @@ class Halfedge {
     this.onBoundary = false // Boolean
   }
   // TODO: you can add more methods if you need here
-
+  vector() {
+    return this.next.vertex.position.sub(this.vertex.position)
+  }
+  cotan() {
+    if (this.onBoundary) {
+      return 0
+    }
+    const u = this.prev.vector()
+    const v = this.next.vector().scale(-1)
+    return u.dot(v) / u.cross(v).norm()
+  }
+  angle() {
+    const u = this.prev.vector().unit()
+    const v = this.next.vector().scale(-1).unit()
+    return Math.acos(Math.max(-1, Math.min(1, u.dot(v))))
+  }
 }
 
 class Edge {
@@ -71,10 +86,33 @@ class Face {
   //    f.vertices(v => { ... })
   vertices(fn) {
     // TODO: iterate all vertices.
-
+    let start = true
+    let i = 0
+    for (let h = this.halfedge; start || h != this.halfedge; h = h.next) {
+      fn(h.vertex, i)
+      start = false
+      i++
+    }
   }
   // TODO: you can add more methods if you need here
-
+  normal() {
+    if (this.halfedge.onBoundary) {
+      return new Vector(0, 0, 0)
+    }
+    const h = this.halfedge
+    let a = h.vertex.position.sub(h.next.vertex.position)
+    let b = h.prev.vertex.position.sub(h.vertex.position).scale(-1)
+    return a.cross(b).unit()
+  }
+  area() {
+    const h = this.halfedge
+    if (h.onBoundary) {
+      return 0
+    }
+    let a = h.vertex.position.sub(h.next.vertex.position)
+    let b = h.prev.vertex.position.sub(h.vertex.position).scale(-1)
+    return a.cross(b).norm() * 0.5
+  }
 }
 
 class Vertex {
@@ -84,7 +122,7 @@ class Vertex {
     this.halfedge = null // Halfedge
     this.idx      = -1   // Number
   }
-  normal(method='equal-weighted') {
+  /**normal(method='equal-weighted') {
     let n = new Vector()
     switch (method) {
     case 'equal-weighted':
@@ -117,9 +155,108 @@ class Vertex {
     // default: // undefined
     //   return n
     // }
-  }
+  }**/
   // TODO: you can add more methods if you need here
+  normal(method='equal-weighted') {
+    let n = new Vector()
+    switch (method) {
+      case 'equal-weighted':
+        this.faces(f => { n = n.add(f.normal()) })
+        return n.unit()
 
+      case 'area-weighted':
+        this.faces(f => { n = n.add(f.normal().scale(f.area())) })
+        return n.unit()
+
+      case 'angle-weighted':
+        this.halfedges(h => {
+          n = n.add(h.face.normal().scale(h.next.angle()))
+        })
+        return n.unit()
+
+      default: // undefined
+        return n
+    }
+  }
+  curvature(method='Mean') {
+    // TRICK: curvatures are scaled to obtain better color encoding
+    // See comments in main.js line 145-150.
+    const [k1, k2] = this.principalCurvature()
+    switch (method) {
+      case 'Mean':
+        return ((k1+k2)*0.5)
+      case 'Gaussian':
+        return k1*k2
+      case 'Kmin':
+        return k1*0.1
+      case 'Kmax':
+        return k2*0.1
+      default: // undefined
+        return 0
+    }
+  }
+  // NOTE: you can add more methods if you need here
+  principalCurvature() {
+    const n = this.meanCurvature()
+    const K = this.angleDefect()
+    let H = K > 0 ? n.norm() : -n.norm()
+    // TRICK: scale the mean curvature to emplify the diff
+    H = H*0.001
+    let d = H*H - K
+    d = d <= 0 ? 0 : Math.sqrt(d)
+    return [H-d, H+d]
+  }
+  meanCurvature() {
+    const a = this.voronoiCell()
+    let sum = new Vector()
+    this.halfedges(h => {
+      if (h.onBoundary || h.twin.onBoundary) {
+        return
+      }
+      sum = sum.add(h.vector().scale(h.cotan() + h.twin.cotan()))
+    })
+    return sum.scale(1/(2*a))
+  }
+  angleDefect() {
+    let sum = 0.0
+    this.halfedges(h => {
+      if (h.onBoundary || h.twin.onBoundary) {
+        return
+      }
+      sum += h.next.angle()
+    })
+    return 2 * Math.PI - sum
+  }
+  voronoiCell() {
+    let a = 0
+    this.halfedges(h => {
+      const u = h.prev.vector().norm()
+      const v = h.vector().norm()
+      a += (u*u*h.prev.cotan() + v*v*h.cotan()) / 8
+    })
+    return a
+  }
+  faces(fn) {
+    let start = true
+    let i = 0
+    for (let h = this.halfedge; start || h != this.halfedge; h = h.twin.next) {
+      if(h.onBoundary) {
+        continue
+      }
+      fn(h.face, i)
+      start = false
+      i++
+    }
+  }
+  halfedges(fn) {
+    let start = true
+    let i = 0
+    for (let h = this.halfedge; start || h != this.halfedge; h = h.twin.next) {
+      fn(h, i)
+      start = false
+      i++
+    }
+  }
 }
 
 export class HalfedgeMesh {
@@ -128,6 +265,17 @@ export class HalfedgeMesh {
    *
    * @param {string} data is a text string from an .obj file
    */
+  /**constructor(data) {
+    // properties we plan to cache
+    this.vertices  = [] // an array of Vertex object
+    this.edges     = [] // an array of Edge object
+    this.faces     = [] // an array of Face object
+    this.halfedges = [] // an array of Halfedge object
+    this.boundaries= [] // an array of boundary loops
+
+    // TODO: read .obj format and construct its halfedge representation
+
+  }**/
   constructor(data) {
     // properties we plan to cache
     this.vertices  = [] // an array of Vertex object
@@ -138,7 +286,193 @@ export class HalfedgeMesh {
 
     // TODO: read .obj format and construct its halfedge representation
 
+    // load .obj file
+    let indices   = []
+    let positions = []
+    let lines = data.split('\n')
+    for (let line of lines) {
+      line = line.trim()
+      const tokens = line.split(' ')
+      switch(tokens[0].trim()) {
+        case 'v':
+          positions.push(new Vector(
+              parseFloat(tokens[1]), parseFloat(tokens[2]), parseFloat(tokens[3]),
+          ))
+          continue
+        case 'f':
+          // only load indices of vertices
+          for (let i = 1; i < tokens.length; i++) {
+            indices.push(parseInt((tokens[i].split('/')[0]).trim()) - 1)
+          }
+          continue
+      }
+    }
+
+    // build the halfedge connectivity
+    const edges = new Map()
+    for (let i = 0; i < indices.length; i += 3) {
+      for (let j = 0; j < 3; j++) { // check a face
+        let a = indices[i + j]
+        let b = indices[i + (j+1)%3]
+
+        if (a > b) {
+          const tmp = b
+          b = a
+          a = tmp
+        }
+
+        // store the edge if not exists
+        const e = `${a}-${b}`
+        if (!edges.has(e)) {
+          edges.set(e, [a, b])
+        }
+      }
+    }
+
+    this.vertices   = new Array(positions.length)
+    this.edges      = new Array(edges.size)
+    this.faces      = new Array(indices.length / 3)
+    this.halfedges  = new Array(edges.size*2)
+
+    const idx2vert = new Map()
+    for (let i = 0; i < positions.length; i++) {
+      const v = new Vertex()
+      v.position = positions[i]
+      this.vertices[i] = v
+      idx2vert.set(i, v)
+    }
+
+    let eidx = 0
+    let existedHe = new Map()
+    let hasTwin = new Map()
+
+    // construct halfedges, edges
+    for (let i = 0; i < indices.length; i += 3) {
+      // construct face
+      const f = new Face()
+      this.faces[i / 3] = f
+
+      // construct halfedges of the face
+      for (let j = 0; j < 3; j++) {
+        const he = new Halfedge()
+        this.halfedges[i+j] = he
+      }
+
+      // construct connectivities of the new halfedges
+      for (let j = 0; j < 3; j++) {
+        // halfedge from vertex a to vertex b
+        let a = indices[i + j]
+        let b = indices[i + (j+1)%3]
+
+        // halfedge properties
+        const he = this.halfedges[i + j]
+        he.next = this.halfedges[i + (j+1)%3]
+        he.prev = this.halfedges[i + (j+2)%3]
+        he.onBoundary = false
+        hasTwin.set(he, false)
+
+        const v = idx2vert.get(a)
+        he.vertex = v
+        v.halfedge = he
+
+        he.face = f
+        f.halfedge = he
+
+        // swap if index a > b, for twin checking
+        if (a > b) {
+          const tmp = b
+          b = a
+          a = tmp
+        }
+        const ek = `${a}-${b}`
+        if (existedHe.has(ek)) {
+          // if a halfedge has been created before, then
+          // it is the twin halfedge of the current halfedge
+          const twin = existedHe.get(ek)
+          he.twin = twin
+          twin.twin = he
+          he.edge = twin.edge
+
+          hasTwin.set(he, true)
+          hasTwin.set(twin, true)
+        } else {
+          // new halfedge
+          const e = new Edge()
+          this.edges[eidx] = e
+          eidx++
+          he.edge = e
+          e.halfedge = he
+
+          existedHe.set(ek, he)
+        }
+
+        // FIXME: non-manifold edge count checking
+      }
+    }
+
+    // create boundary halfedges and hidden faces for the boundary
+    let hidx = indices.length
+    for (let i = 0; i < indices.length; i++) {
+      const he = this.halfedges[i]
+      if (hasTwin.get(he)) {
+        continue
+      }
+
+      // handle halfedge that has no twin
+      const f = new Face() // hidden face
+      let bcycle = []      // boundary cycle
+      let current = he
+      do {
+        const bhe = new Halfedge() // boundary halfedge
+        this.halfedges[hidx] = bhe
+        hidx++
+        bcycle.push(bhe)
+
+        // grab the next halfedge along the boundary that does not
+        // have a twin halfedge
+        let next = current.next
+        while (hasTwin.get(next)) {
+          next = next.twin.next
+        }
+
+        // set the current halfedge's attributes
+        bhe.vertex = next.vertex
+        bhe.edge = current.edge
+        bhe.onBoundary = true
+
+        // point the new halfedge and face to each other
+        bhe.face = f
+        f.halfedge = bhe
+
+        // point the new halfedge and twin to each other
+        bhe.twin = current
+        current.twin = bhe
+
+        current = next
+      } while(current != he)
+
+      // link the cycle of boundary halfedges together
+      const n = bcycle.length
+      for (let j = 0; j < n; j++) {
+        bcycle[j].next = bcycle[(j+n-1)%n]
+        bcycle[j].prev = bcycle[(j+1)%n]
+        hasTwin.set(bcycle[j], true)
+        hasTwin.set(bcycle[j].twin, true)
+      }
+    }
+
+    // reset indices
+    let index = 0
+    this.vertices.forEach(v => { v.idx = index++ })
+    index = 0
+    this.edges.forEach(e => { e.idx = index++ })
+    index = 0
+    this.faces.forEach(f => { f.idx = index++ })
+    index = 0
+    this.halfedges.forEach(h => { h.idx = index++ })
   }
+
+
   /**
    * flatten computes the UV coordinates of the given triangle mesh.
    *
