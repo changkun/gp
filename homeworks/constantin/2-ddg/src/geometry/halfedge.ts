8b8e0753e8b4822dba62d458c09eabcd09b15a62
ru26kif <constantin.geier@campus.lmu.de>
2021-11-09 18:09:32 +0100

    copy the halfedge code into the repo to get some output - note using changcungs solution for halfedge for now

--- homeworks/constantin/2-ddg/src/geometry/halfedge.ts
+++ homeworks/constantin/2-ddg/src/geometry/halfedge.ts
@@ -70,7 +70,173 @@ export class HalfedgeMesh {
    * @param positions is the vertex buffer that contains all vertex positions.
    */
   buildMesh(indices: number[], positions: Vector[]) {
-    // TODO: use the halfedge structrue implementation from Homework 1.
     // We assume the input mesh is a manifold mesh.
+     // build the halfedge connectivity
+     const edges = new Map()
+     for (let i = 0; i < indices.length; i += 3) {
+       for (let j = 0; j < 3; j++) { // check a face
+         var a = indices[i + j]
+         var b = indices[i + (j+1)%3]
+ 
+         if (a > b) {
+           const tmp = b
+           b = a
+           a = tmp
+         }
+ 
+         // store the edge if not exists
+         const e = `${a}-${b}`
+         if (!edges.has(e)) {
+           edges.set(e, [a, b])
+         }
+       }
+     }
+ 
+     this.verts   = new Array(positions.length)
+     this.edges      = new Array(edges.size)
+     this.faces      = new Array(indices.length / 3)
+     this.halfedges  = new Array(edges.size*2)
+ 
+     const idx2vert = new Map()
+     for (let i = 0; i < positions.length; i++) {
+       const v = new Vertex(positions[i])
+       //v.position = positions[i]
+       this.verts[i] = v
+       idx2vert.set(i, v)
+     }
+ 
+     let eidx = 0
+     let existedHe = new Map()
+     let hasTwin = new Map()
+ 
+     // construct halfedges, edges
+     for (let i = 0; i < indices.length; i += 3) {
+       // construct face
+       const f = new Face()
+       this.faces[i / 3] = f
+ 
+       // construct halfedges of the face
+       for (let j = 0; j < 3; j++) {
+         const he = new Halfedge()
+         this.halfedges[i+j] = he
+       }
+ 
+       // construct connectivities of the new halfedges
+       for (let j = 0; j < 3; j++) {
+         // halfedge from vertex a to vertex b
+         var a = indices[i + j]
+         var b = indices[i + (j+1)%3]
+ 
+         // halfedge properties
+         const he = this.halfedges[i + j]
+         he.next = this.halfedges[i + (j+1)%3]
+         he.prev = this.halfedges[i + (j+2)%3]
+         he.onBoundary = false
+         hasTwin.set(he, false)
+ 
+         const v = idx2vert.get(a)
+         he.vert = v
+         v.halfedge = he
+ 
+         he.face = f
+         f.halfedge = he
+ 
+         // swap if index a > b, for twin checking
+         if (a > b) {
+           const tmp = b
+           b = a
+           a = tmp
+         }
+         const ek = `${a}-${b}`
+         if (existedHe.has(ek)) {
+           // if a halfedge has been created before, then
+           // it is the twin halfedge of the current halfedge
+           const twin = existedHe.get(ek)
+           he.twin = twin
+           twin.twin = he
+           he.edge = twin.edge
+ 
+           hasTwin.set(he, true)
+           hasTwin.set(twin, true)
+         } else {
+           // new halfedge
+           const e = new Edge()
+           this.edges[eidx] = e
+           eidx++
+           he.edge = e
+           e.halfedge = he
+ 
+           existedHe.set(ek, he)
+         }
+ 
+         // FIXME: non-manifold edge count checking
+       }
+     }
+ 
+     // create boundary halfedges and hidden faces for the boundary
+     let hidx = indices.length
+     for (let i = 0; i < indices.length; i++) {
+       const he = this.halfedges[i]
+       if (hasTwin.get(he)) {
+         continue
+       }
+ 
+       // handle halfedge that has no twin
+       const f = new Face() // hidden face
+       let bcycle = []      // boundary cycle
+       let current = he
+       do {
+         const bhe = new Halfedge() // boundary halfedge
+         this.halfedges[hidx] = bhe
+         hidx++
+         bcycle.push(bhe)
+ 
+         // grab the next halfedge along the boundary that does not
+         // have a twin halfedge
+         var next = current.next;
+
+         while (hasTwin.get(next)) {
+           next = next?.twin?.next;
+         }
+ 
+         // set the current halfedge's attributes
+         bhe.vert = next?.vert;
+         bhe.edge = current.edge
+         bhe.onBoundary = true
+ 
+         // point the new halfedge and face to each other
+         bhe.face = f
+         f.halfedge = bhe
+ 
+         // point the new halfedge and twin to each other
+         bhe.twin = current
+         current.twin = bhe
+ 
+         current = next!;
+       } while(current != he)
+ 
+       // link the cycle of boundary halfedges together
+       const n = bcycle.length
+       for (let j = 0; j < n; j++) {
+         bcycle[j].next = bcycle[(j+n-1)%n]
+         bcycle[j].prev = bcycle[(j+1)%n]
+         hasTwin.set(bcycle[j], true)
+         hasTwin.set(bcycle[j].twin, true)
+       }
+     }
+ 
+     // reset indices
+     let index = 0
+     this.verts.forEach(v => { v.idx = index++ })
+     index = 0
+     this.edges.forEach(e => { e.idx = index++ })
+     index = 0
+     this.faces.forEach(f => { f.idx = index++ })
+     index = 0
+     this.halfedges.forEach(h => { h.idx = index++ })
+     
+     console.log("Source:N vertices"+this.verts.length);
+     console.log("Source:N indices"+indices.length);
+     console.log("N faces:"+this.faces.length);
   }
 }
