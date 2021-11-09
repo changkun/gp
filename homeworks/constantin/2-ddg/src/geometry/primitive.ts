2a2093f16c92c66b133e43a69e971336bc645574
ru26kif <constantin.geier@campus.lmu.de>
2021-11-09 18:05:23 +0100

    Initial setup - copy dir for 2nd homweork

---
+++ homeworks/constantin/2-ddg/src/geometry/primitive.ts
@@ -0,0 +1,146 @@
+// Copyright (c) 2021 LMU Munich Geometry Processing Authors. All rights reserved.
+// Created by Changkun Ou <https://changkun.de>.
+//
+// Use of this source code is governed by a GNU GPLv3 license that can be found
+// in the LICENSE file.
+
+import {Vector} from '../linalg/vec';
+
+export class Halfedge {
+  vert?: Vertex;
+  edge?: Edge;
+  face?: Face;
+
+  prev?: Halfedge;
+  next?: Halfedge;
+  twin?: Halfedge;
+
+  idx: number;
+  onBoundary: boolean;
+
+  constructor() {
+    this.idx = -1;
+    this.onBoundary = false;
+  }
+  vector(): Vector {
+    // TODO: compute the edge vector.
+    return new Vector();
+  }
+  cotan(): number {
+    // TODO: Compute the cotan formula at this edge, if an edge
+    // is on the boundary, then return zero.
+    return 0;
+  }
+  angle(): number {
+    // TODO: compute the tip angle at this edge.
+    return 0;
+  }
+}
+
+export class Edge {
+  halfedge?: Halfedge;
+  idx: number;
+
+  constructor() {
+    this.idx = -1;
+  }
+}
+
+export class Face {
+  halfedge?: Halfedge;
+  idx: number;
+
+  constructor() {
+    this.idx = -1;
+  }
+  vertices(fn: (v: Vertex, n: number) => void) {
+    let start = true;
+    let i = 0;
+    for (let h = this.halfedge; start || h !== this.halfedge; h = h!.next) {
+      fn(h!.vert!, i);
+      start = false;
+      i++;
+    }
+  }
+
+  normal(): Vector {
+    // TODO: compute the face normal of this face.
+    return new Vector();
+  }
+  area(): number {
+    // TODO: compute the area of this face.
+    return 0;
+  }
+}
+
+export enum NormalMethod {
+  EqualWeighted = 'Equal Weighted',
+  AreaWeighted = 'Area Weighted',
+  AngleWeighted = 'Angle Weighted',
+}
+
+export enum CurvatureMethod {
+  None = 'None',
+  Mean = 'Mean',
+  Gaussian = 'Caussian',
+  Kmin = 'Kmin',
+  Kmax = 'Kmax',
+}
+
+export class Vertex {
+  position: Vector;
+  halfedge?: Halfedge;
+  idx: number;
+
+  constructor(position: Vector) {
+    this.position = position;
+    this.idx = -1;
+  }
+  faces(fn: (f: Face, i: number) => void) {
+    let start = true;
+    let i = 0;
+    for (
+      let h = this.halfedge;
+      start || h !== this.halfedge;
+      h = h!.twin!.next
+    ) {
+      if (h!.onBoundary) {
+        continue;
+      }
+      fn(h!.face!, i);
+      start = false;
+      i++;
+    }
+  }
+  halfedges(fn: (h: Halfedge, i: number) => void) {
+    let start = true;
+    let i = 0;
+    for (
+      let h = this.halfedge;
+      start || h !== this.halfedge;
+      h = h!.twin!.next
+    ) {
+      fn(h!, i);
+      start = false;
+      i++;
+    }
+  }
+
+  normal(method = NormalMethod.EqualWeighted): Vector {
+    // TODO: compute vertex normal given different method:
+    // 1. EqualWeighted
+    // 2. AreaWeighted
+    // 3. AngleWeighted
+    return new Vector();
+  }
+  curvature(method = CurvatureMethod.Mean): number {
+    // TODO: compute curvature given different method:
+    // 1. None
+    // 2. Mean
+    // 3. Gaussian
+    // 4. Kmin
+    // 5. Kmax
+    return 0;
+  }
+  // NOTE: you can add more methods if needed
+}
