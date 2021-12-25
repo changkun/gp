d5aa23f62aa2b811dc33331492439637be0c03e0
Marcel Quanz <marcel.quanz@gmail.com>
2021-12-25 12:37:24 +0100

    homework: add marlq's solution 03 (#46)
    * First try for backup.
    
    * Current results look correct, but actually is not due to a misunderstanding how twin halfedges work. Duplicates are not correctly found and used.
    
    * Still some problems...
    
    * Current solution seems correct: amount of edges is correct, and pointers seem correct. However, onBoundary is true for some halfedges, which does not seem correct for the mesh.
    
    * Commented out debug.
    
    * Answered the readme questions. Rendered one of the blender bunnies (but the freestyle rendering might cause problems later).
    
    * Another try at the wireframe bunny, this time with the wireframe modifier. For some reason I can't copy the bunny, or move the copy. Moving the camera is also pain.
    
    * First try for backup.
    
    * Still some problems...
    
    * Commented out debug.
    
    * Set up files.
    
    * Updated code of buildMesh to build necessary twin halfedges on the boundaries of the mesh to accomodate for code changes.
    
    * Assigned vertsOrig so that the various normal and curvature modes work correctly.
    
    * First try at implementing the (for now only Uniform) Laplacian Smoothing. Still don't understand the mathematical definitions of everything. Resulting mesh is heavily deformed.
    
    * Moved the neighbor calculation to primitive.ts. Changed the smoothing formula to use the Laplace matrix instead of weight and mass matrix. Everything looks correct when printing the matrices, but the bunny is still totally broken.
    
    * Tried to implement the cotan version of Laplacian smooth. Of course it is just a mess as the uniform one.
    
    * With the correct usage of timeStep, and reverting back to using the Mass matrix, instead of D, the Uniform solution looks somewhat correct now. Cotan is still totally broken.
    
    * Fixed the cotan version to use the cotan of the correct halfedges. It still smooth much too fast though, and the mass matrix uses the inverse of the vertex area, which seems to be wrong...
    
    * Multiplied the mass matrix by 100 entries for cotan as the suggestion from changkun. It smooths a lot slower now and is more stable.
    
    * Wrote down some notes for the additional tasks.
    
    * Wrote something better for the additional tasks. Changed the vertex area to the voronoi cell, so that the verts do not have to be reset.
    
    * Initial commit for homework 4.
    
    * First attempt at a solution for the Tutte embedding. The results are horrific. In theory everything should work, but there might be a mistake somewhere.
    
    * Swapped cholesky solver with another. Changed the square mapping, but it's still awful. Implemented the cotan matrix.
    
    * Fixed the square boundary.

---
+++ homeworks/MarlQ/3-smooth/src/geometry/halfedge.ts
@@ -0,0 +1,414 @@
+// Copyright (c) 2021 LMU Munich Geometry Processing Authors. All rights reserved.
+// Created by Changkun Ou <https://changkun.de>.
+//
+// Use of this source code is governed by a GNU GPLv3 license that can be found
+// in the LICENSE file.
+
+// API Usage about @penrose/linear-algebra:
+//
+//   - There are two types of matrices: SparseMatrix and DenseMatrix
+//   - SparseMatrix.identity(n, n) gives you a identity matrix with
+//     n x n dimension
+//   - Triplet represents a small structure to hold non-zero entries in
+//     SparseMatrix, each entry is (x, i, j). To construct a SparseMatrix,
+//     here is an example:
+//
+//       let A = new Triplet(2, 2)          // Triplet for 2x2 SparseMatrix
+//       A.addEntry(1, 0, 0)                // A(0, 0) += 1
+//       A.addEntry(2, 1, 1)                // A(1, 1) += 2
+//       return SparseMatrix.fromTriplet(T) // Construct SparseMatrix
+//
+//   - A.timesSparse(B) returns A*B where A and B are SparseMatrix.
+//   - A.plus(B) returns A+B where A and B are SparseMatrix.
+//   - A.timesReal(s) returns sA where A is SparseMatrix and s is a real number.
+//   - A.chol() returns a sparse Cholesky decomposition.
+//   - A.solvePositiveDefinite(b) solves linear equation Ax=b where
+//     A is a Cholesky decomposition, and b is a DenseMatrix, and x is the solution.
+//   - For a DenseMatrix A, one can use A.set(x, i, j) for A(i,j)=x,
+//     and A.get(i, j) returns A(i,j).
+//
+// Further APIs regarding @penrose/linear-algebra can be found
+// in node_modules/@penrose/linear-algebra/docs/*.html, but the above
+// information are all the APIs you need for this project.
+import {SparseMatrix, DenseMatrix, Triplet} from '@penrose/linear-algebra';
+import {Vertex, Edge, Face, Halfedge} from './primitive';
+import {Vector} from '../linalg/vec';
+import { OneFactor } from 'three';
+
+export enum WeightType {
+  Uniform = 'Uniform',
+  Cotan = 'Cotan',
+}
+
+export class HalfedgeMesh {
+  color: Vector;
+  wireframe: Vector;
+
+  // The following four fields are the key fields to represent half-edge based
+  // meshes.
+  vertsOrig: Vertex[]; // The original copy of all vertex positions
+  verts: Vertex[]; // The current vertex that are updated after smooth for actual rendering
+  edges: Edge[]; // a list of edges
+  faces: Face[]; // a list of faces
+  halfedges: Halfedge[]; // a list of halfedges
+
+  /**
+   * constructor constructs the halfedge-based mesh representation.
+   *
+   * @param {string} data is a text string from an .obj file
+   */
+  constructor(data: string) {
+    this.color = new Vector(0, 128, 255, 1);
+    this.wireframe = new Vector(125, 125, 125, 1);
+
+    // load .obj file
+    const indices: number[] = [];
+    const positions: Vector[] = [];
+    const lines = data.split('\n');
+    for (let line of lines) {
+      line = line.trim();
+      const tokens = line.split(' ');
+      switch (tokens[0].trim()) {
+        case 'v':
+          positions.push(
+            new Vector(
+              parseFloat(tokens[1]),
+              parseFloat(tokens[2]),
+              parseFloat(tokens[3]),
+              1
+            )
+          );
+          break;
+        case 'f':
+          // only load indices of vertices
+          for (let i = 1; i < tokens.length; i++) {
+            const vv = tokens[i].split('/');
+            indices.push(parseInt(vv[0]) - 1);
+          }
+          break;
+      }
+    }
+
+    this.vertsOrig = [];
+    this.verts = [];
+    this.edges = [];
+    this.faces = [];
+    this.halfedges = [];
+    this.buildMesh(indices, positions);
+  }
+
+  /**
+   * buildMesh builds half-edge based connectivity for the given vertex index buffer
+   * and vertex position buffer.
+   *
+   * @param indices is the vertex index buffer that contains all vertex indices.
+   * @param positions is the vertex buffer that contains all vertex positions.
+   */
+   buildMesh(indices: number[], positions: Vector[]) {
+    const edges = new Map();
+    for (let i = 0; i < indices.length; i += 3) {
+      for (let j = 0; j < 3; j++) {
+        // check a face
+        let a = indices[i + j];
+        let b = indices[i + ((j + 1) % 3)];
+
+        if (a > b) {
+          const tmp = b;
+          b = a;
+          a = tmp;
+        }
+
+        // store the edge if not exists
+        const e = `${a}-${b}`;
+        if (!edges.has(e)) {
+          edges.set(e, [a, b]);
+        }
+      }
+    }
+
+    this.verts = new Array(positions.length);
+    this.edges = new Array(edges.size);
+    this.faces = new Array(indices.length / 3);
+    this.halfedges = new Array(edges.size * 2);
+
+    const idx2vert = new Map();
+    for (let i = 0; i < positions.length; i++) {
+      let v = new Vertex(positions[i]);
+      this.verts[i] = v;
+      idx2vert.set(i, v);
+
+      v = new Vertex(new Vector(positions[i].x, positions[i].y, positions[i].z));
+
+      this.vertsOrig[i] = v;
+    }
+
+    let eidx = 0;
+    const existedHe = new Map();
+    const hasTwin = new Map();
+
+    // construct halfedges, edges
+    for (let i = 0; i < indices.length; i += 3) {
+      // construct face
+      const f = new Face();
+      this.faces[i / 3] = f;
+
+      // construct halfedges of the face
+      for (let j = 0; j < 3; j++) {
+        const he = new Halfedge();
+        this.halfedges[i + j] = he;
+      }
+
+      // construct connectivities of the new halfedges
+      for (let j = 0; j < 3; j++) {
+        // halfedge from vertex a to vertex b
+        let a = indices[i + j];
+        let b = indices[i + ((j + 1) % 3)];
+
+        // halfedge properties
+        const he = this.halfedges[i + j];
+        he.next = this.halfedges[i + ((j + 1) % 3)];
+        he.prev = this.halfedges[i + ((j + 2) % 3)];
+        he.onBoundary = false;
+        hasTwin.set(he, false);
+
+        const v = idx2vert.get(a);
+        he.vert = v;
+        v.halfedge = he;
+
+        he.face = f;
+        f.halfedge = he;
+
+        // swap if index a > b, for twin checking
+        if (a > b) {
+          const tmp = b;
+          b = a;
+          a = tmp;
+        }
+        const ek = `${a}-${b}`;
+        if (existedHe.has(ek)) {
+          // if a halfedge has been created before, then
+          // it is the twin halfedge of the current halfedge
+          const twin = existedHe.get(ek);
+          he.twin = twin;
+          twin.twin = he;
+          he.edge = twin.edge;
+
+          hasTwin.set(he, true);
+          hasTwin.set(twin, true);
+        } else {
+          // new halfedge
+          const e = new Edge();
+          this.edges[eidx] = e;
+          eidx++;
+          he.edge = e;
+          e.halfedge = he;
+
+          existedHe.set(ek, he);
+        }
+      }
+    }
+
+    // create boundary halfedges and hidden faces for the boundary
+    let hidx = indices.length;
+    for (let i = 0; i < indices.length; i++) {
+      const he = this.halfedges[i];
+      if (hasTwin.get(he)) {
+        continue;
+      }
+
+      // handle halfedge that has no twin
+      const f = new Face(); // hidden face
+      const bcycle = []; // boundary cycle
+      let current = he;
+      do {
+        const bhe = new Halfedge(); // boundary halfedge
+        this.halfedges[hidx] = bhe;
+        hidx++;
+        bcycle.push(bhe);
+
+        // grab the next halfedge along the boundary that does not
+        // have a twin halfedge
+        let next = <Halfedge>current.next;
+        while (hasTwin.get(next)) {
+          next = <Halfedge>next.twin!.next;
+        }
+
+        // set the current halfedge's attributes
+        bhe.vert = next.vert;
+        bhe.edge = current.edge;
+        bhe.onBoundary = true;
+
+        // point the new halfedge and face to each other
+        bhe.face = f;
+        f.halfedge = bhe;
+
+        // point the new halfedge and twin to each other
+        bhe.twin = current;
+        current.twin = bhe;
+
+        current = next;
+      } while (current !== he);
+
+      // link the cycle of boundary halfedges together
+      const n = bcycle.length;
+      for (let j = 0; j < n; j++) {
+        bcycle[j].next = bcycle[(j + n - 1) % n];
+        bcycle[j].prev = bcycle[(j + 1) % n];
+        hasTwin.set(bcycle[j], true);
+        hasTwin.set(bcycle[j].twin, true);
+      }
+    }
+
+    // reset indices
+    let index = 0;
+    this.verts.forEach(v => {
+      v.idx = index++;
+    });
+    index = 0;
+    this.edges.forEach(e => {
+      e.idx = index++;
+    });
+    index = 0;
+    this.faces.forEach(f => {
+      f.idx = index++;
+    });
+    index = 0;
+    this.halfedges.forEach(h => {
+      h.idx = index++;
+      h.vertsOrig = this.vertsOrig;
+    });
+  }
+
+  /**
+   * smooth performs the Laplacian smoothing algorithm.
+   * @param weightType indicates the type of the weight for
+   * constructing the Laplace matrix. Possible value could be: 'uniform',
+   * 'cotan'.
+   * @param timeStep the time step in Laplacian Smoothing algorithm
+   * @param smoothStep the smooth step in Laplacian Smoothing algorithm
+   */
+  smooth(weightType: WeightType, timeStep: number, smoothStep: number) {
+
+    // Reset vertices to original positions
+    /* this.vertsOrig.forEach((v, index) => {
+      this.verts[index].position.x = v.position.x;
+      this.verts[index].position.y = v.position.y;
+      this.verts[index].position.z = v.position.z;
+    }); */
+
+
+    //   1. Build f(t)
+    let f = DenseMatrix.zeros(this.verts.length, 3);
+    for(let i = 0; i < this.verts.length; i++) {
+      f.set(this.vertsOrig[i].position.x, i, 0);
+      f.set(this.vertsOrig[i].position.y, i, 1);
+      f.set(this.vertsOrig[i].position.z, i, 2);
+    }
+    
+    //   2. Build the mass matrix `M`
+    let M = new Triplet(this.verts.length, this.verts.length);          
+
+
+    if(weightType === 'Uniform') {
+    for(let i = 0; i < this.verts.length; i++) {
+        let neighbor_verts = this.verts[i].getNeighbors();
+        M.addEntry(neighbor_verts.length, i, i);
+      }
+    }
+    else if(weightType === 'Cotan') {
+      for(let i = 0; i < this.verts.length; i++) {
+        M.addEntry( this.verts[i].voronoiCell() * 100, i, i); // Alternatively: this.verts[i].vertexArea() produces very similar results
+      }
+    }
+
+    const massMatrix = SparseMatrix.fromTriplet(M);
+
+    //   3. Build the Laplace weight matrix `W` for the given `weightType` in laplaceWeightMatrix
+    const weightMatrix = this.laplaceWeightMatrix(weightType);
+    
+    //   4. Solve linear system (M - tλW)f' = Mf using a Cholesky solver.
+    let A = massMatrix.plus(weightMatrix.timesReal(-1.0 * smoothStep * timeStep));
+    let b = massMatrix.timesDense(f);
+
+    // Question: Why did this solution not work? (With adjusting the mass matrix to be D = M^-1)
+    //let L = massMatrix.timesSparse(weightMatrix); // L = DW
+    //let A = SparseMatrix.identity(this.verts.length, this.verts.length).plus(L.timesReal(-1.0 * smoothStep * timeStep)); // (I - lambda * h * L)
+    //let b = f;
+
+    const f_new = A.chol().solvePositiveDefinite(b);
+    console.log(f_new)
+
+    //   5. Update the position of mesh vertices based on the solution f'.
+    for(let i = 0; i < this.verts.length; i++) {
+      this.verts[i].position.x = f_new.get(i, 0);
+      this.verts[i].position.y = f_new.get(i, 1);
+      this.verts[i].position.z = f_new.get(i, 2);
+    }
+
+    /* let y = weightMatrix.toDense();
+    let txt = '';
+    for(let a = 0; a < y.nRows(); a++ ) {
+      for(let b = 0; b < y.nCols(); b++ ) {
+        txt += y.get(a,b) + '  ';
+      }
+      console.log(txt)
+      txt = '';
+    } */
+
+    
+    
+  }
+
+  /**
+   * laplaceWeightMatrix returns the Laplace weight matrix for a given laplaceType
+   * @param weightType indicates the type of the weight for
+   * constructing the Laplace matrix.
+   */
+  laplaceWeightMatrix(weightType: WeightType) {
+    //
+    // Hint: To avoid numeric issue when solving linear equation,
+    // add 1e-8 to all elements.
+
+    const W = new Triplet(this.verts.length, this.verts.length);   
+
+    if(weightType === 'Uniform') {
+      for(let i = 0; i < this.verts.length; i++) {
+        // Calculate neighbor vertices for vertex i
+        let neighbor_verts = this.verts[i].getNeighbors();
+        
+        // Set uniform neighbor weights
+        neighbor_verts.forEach(v => {
+          W.addEntry(1 + 1e-8, i, v!.idx);
+        });
+        //console.assert(i === this.verts[i].idx)
+        // Set own weight
+        W.addEntry( -neighbor_verts.length + 1e-8, i, i);
+        
+      }
+    }
+    else if(weightType === 'Cotan') {
+      for(let i = 0; i < this.verts.length; i++) {
+        
+        // TODO: This code is a bit lazily done
+        let neighbor_verts = this.verts[i].getNeighbors();
+        let sum = 0;
+        neighbor_verts.forEach(v => {
+
+          // Get the halfedge between vertices i and neighbor v
+          let halfedge = v.halfedge;
+          while(halfedge!.twin!.vert != this.verts[i]) {
+            halfedge = halfedge!.twin!.next;
+          }
+
+          const cot = 0.5 * ( halfedge!.cotan() + halfedge!.twin!.cotan() ); // 0.5 * cot alpha + cotan beta
+          sum += cot;
+          W.addEntry( cot + 1e-8, i, v!.idx);
+        });
+
+        W.addEntry( -sum + 1e-8, i, i); // - sum ( weights )
+      }
+    }
+    const weightMatrix = SparseMatrix.fromTriplet(W);
+    return weightMatrix;
+  }
+}
---
+++ homeworks/MarlQ/3-smooth/src/geometry/primitive.ts
@@ -0,0 +1,256 @@
+// Copyright (c) 2021 LMU Munich Geometry Processing Authors. All rights reserved.
+// Created by Changkun Ou <https://changkun.de>.
+//
+// Use of this source code is governed by a GNU GPLv3 license that can be found
+// in the LICENSE file.
+
+import {Vector} from '../linalg/vec';
+
+export class Halfedge {
+  vertsOrig?: Vertex[];
+  vert?: Vertex;
+  edge?: Edge;
+  face?: Face;
+
+  prev?: Halfedge;
+  next?: Halfedge;
+  twin?: Halfedge;
+
+  idx: number;
+  onBoundary: boolean;
+
+  constructor() {
+    this.idx = -1;
+    this.onBoundary = false;
+  }
+  vector(): Vector {
+    // HACK: using the original vertex
+    const a = this.vertsOrig![this.next!.vert!.idx];
+    const b = this.vertsOrig![this.vert!.idx];
+    return a.position.sub(b.position);
+  }
+  cotan(): number {
+    // Compute the cotan formula at this edge, if an edge
+    // is on the boundary, then return zero.
+    if (this.onBoundary) {
+      return 0;
+    }
+    const u = this.prev!.vector();
+    const v = this.next!.vector().scale(-1);
+    return u.dot(v) / u.cross(v).len();
+  }
+  angle(): number {
+    // Compute the tip angle at this edge.
+    const u = this.prev!.vector().unit();
+    const v = this.next!.vector().scale(-1).unit();
+    return Math.acos(Math.max(-1, Math.min(1, u.dot(v))));
+  }
+}
+
+export class Edge {
+  halfedge?: Halfedge;
+  idx: number;
+
+  constructor() {
+    this.idx = -1;
+  }
+}
+
+export class Face {
+  halfedge?: Halfedge;
+  idx: number;
+
+  constructor() {
+    this.idx = -1;
+  }
+  vertices(fn: (v: Vertex, n: number) => void) {
+    let start = true;
+    let i = 0;
+    for (let h = this.halfedge; start || h !== this.halfedge; h = h!.next) {
+      fn(h!.vert!, i);
+      start = false;
+      i++;
+    }
+  }
+
+  normal(): Vector {
+    // Compute the face normal of this face.
+    if (this.halfedge!.onBoundary) {
+      return new Vector(0, 0, 0);
+    }
+    const h = this.halfedge!;
+    const a = h.vert!.position.sub(h.next!.vert!.position);
+    const b = h.prev!.vert!.position.sub(h.vert!.position).scale(-1);
+    return a.cross(b).unit();
+  }
+  area(): number {
+    // Compute the area of this face.
+    const h = this.halfedge!;
+    if (h.onBoundary) {
+      return 0;
+    }
+    const a = h.vert!.position.sub(h.next!.vert!.position);
+    const b = h.prev!.vert!.position.sub(h.vert!.position).scale(-1);
+    return a.cross(b).len() * 0.5;
+  }
+}
+
+export enum NormalMethod {
+  EqualWeighted = 'Equal Weighted',
+  AreaWeighted = 'Area Weighted',
+  AngleWeighted = 'Angle Weighted',
+}
+
+export enum CurvatureMethod {
+  None = 'None',
+  Mean = 'Mean',
+  Gaussian = 'Caussian',
+  Kmin = 'Kmin',
+  Kmax = 'Kmax',
+}
+
+export class Vertex {
+  position: Vector;
+  halfedge?: Halfedge;
+  idx: number;
+
+  constructor(position: Vector) {
+    this.position = position;
+    this.idx = -1;
+  }
+  faces(fn: (f: Face, i: number) => void) {
+    let start = true;
+    let i = 0;
+    for (
+      let h = this.halfedge;
+      start || h !== this.halfedge;
+      h = h!.twin!.next
+    ) {
+      if (h!.onBoundary) {
+        continue;
+      }
+      fn(h!.face!, i);
+      start = false;
+      i++;
+    }
+  }
+  halfedges(fn: (h: Halfedge, i: number) => void) {
+    let start = true;
+    let i = 0;
+    for (
+      let h = this.halfedge;
+      start || h !== this.halfedge;
+      h = h!.twin!.next
+    ) {
+      fn(h!, i);
+      start = false;
+      i++;
+    }
+  }
+
+  normal(method = NormalMethod.EqualWeighted): Vector {
+    // Compute vertex normal given different method:
+    // 1. EqualWeighted
+    // 2. AreaWeighted
+    // 3. AngleWeighted
+    let n = new Vector();
+    switch (method) {
+      case NormalMethod.EqualWeighted:
+        this.faces(f => {
+          n = n.add(f.normal());
+        });
+        return n.unit();
+
+      case NormalMethod.AreaWeighted:
+        this.faces(f => {
+          n = n.add(f.normal().scale(f.area()));
+        });
+        return n.unit();
+
+      case NormalMethod.AngleWeighted:
+        this.halfedges(h => {
+          n = n.add(h.face!.normal().scale(h.next!.angle()));
+        });
+        return n.unit();
+    }
+  }
+  curvature(method = CurvatureMethod.Mean): number {
+    // Compute curvature given different method:
+    // 1. None
+    // 2. Mean
+    // 3. Gaussian
+    // 4. Kmin
+    // 5. Kmax
+    const [k1, k2] = this.principalCurvature();
+    switch (method) {
+      case CurvatureMethod.Mean:
+        return (k1 + k2) * 0.5;
+      case CurvatureMethod.Gaussian:
+        return k1 * k2;
+      case CurvatureMethod.Kmin:
+        return k1 * 0.1;
+      case CurvatureMethod.Kmax:
+        return k2 * 0.1;
+      case CurvatureMethod.None:
+        return 0;
+    }
+  }
+  // NOTE: you can add more methods if you need here
+  principalCurvature() {
+    const n = this.meanCurvature();
+    const K = this.angleDefect();
+    const H = K > 0 ? n.len() : -n.len();
+
+    let d = H * H - K;
+    d = d <= 0 ? 0 : Math.sqrt(d);
+    return [H - d, H + d];
+  }
+  meanCurvature(): Vector {
+    const a = this.voronoiCell();
+    let sum = new Vector();
+    this.halfedges(h => {
+      if (h.onBoundary || h.twin!.onBoundary) {
+        return;
+      }
+      sum = sum.add(h.vector().scale(h.cotan() + h.twin!.cotan()));
+    });
+    return sum.scale(1 / (2 * a));
+  }
+  angleDefect(): number {
+    let sum = 0.0;
+    this.halfedges(h => {
+      if (h.onBoundary || h.twin!.onBoundary) {
+        return;
+      }
+      sum += h.next!.angle();
+    });
+    return 2 * Math.PI - sum;
+  }
+  voronoiCell(): number {
+    let a = 0;
+    this.halfedges(h => {
+      const u = h.prev!.vector().len();
+      const v = h.vector().len();
+      a += (u * u * h.prev!.cotan() + v * v * h.cotan()) / 8;
+    });
+    return a;
+  }
+
+  getNeighbors(): Vertex[] {
+    const e0 = this.halfedge;
+    const neighbor_verts = [e0!.twin!.vert!];
+    for(let e = e0!.twin!.next!.twin!; e != e0!.twin; e = e!.next!.twin!) {
+      neighbor_verts.push(e.vert!);
+    }
+    return neighbor_verts;
+  }
+
+  vertexArea(): number {
+    const neighbors = this.getNeighbors();
+    let area = 0;
+    neighbors.forEach(n => {
+      area += n.halfedge!.face!.area();
+    });
+    return (1/3) * area;
+  }
+}
