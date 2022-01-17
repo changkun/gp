c811dd19d4eb092c3a1de8ae44dceb89c7ee47a5
consti10 <geierconstantinabc@gmail.com>
2022-01-17 17:19:05 +0100

    work on it

--- homeworks/constantin/6-dda1/main.py
+++ homeworks/constantin/6-dda1/main.py
@@ -19,15 +19,81 @@ from pytorch3d.renderer import (
 )
 import matplotlib.pyplot as plt
 
-device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-torch.cuda.set_device(device)
+print("Begin")
+
+#device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+#torch.cuda.set_device(device)
+"""if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+    print("CUDA yes")
+else:
+    device = torch.device("cpu")
+    print("CUDA no")"""
+# Consti10 - I was not able to pytorch3D with CUDA enabled. But I am running in linux (since windows didn't work)    
+# which has cuda - therefore the above code won't work
+device = torch.device("cpu")
+
 
 mesh = load_objs_as_meshes([os.path.join('./data', 'bunny.obj')], device=device)
+print("Done loading mesh")
 
 # TODO: render the loaded mesh using the already imported classes and functions
+#plt.figure(figsize=(7,7))
+#texture_image=mesh.textures.maps_padded()
+#plt.imshow(texture_image.squeeze().cpu().numpy())
+#plt.axis("off");
 
+# ---------------------------------
+# Initialize a camera.
+# With world coordinates +Y up, +X left and +Z in, the front of the cow is facing the -Z direction. 
+# So we move the camera by 180 in the azimuth direction so it is facing the front of the cow. 
+# MODIFIED for assignment
+#R, T = look_at_view_transform(2.7, 0, 180) 
+R, T = look_at_view_transform(4, 4, 80) 
+cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
+
+# Define the settings for rasterization and shading. Here we set the output image to be of size
+# 512x512. As we are rendering images for visualization purposes only we will set faces_per_pixel=1
+# and blur_radius=0.0. We also set bin_size and max_faces_per_bin to None which ensure that 
+# the faster coarse-to-fine rasterization method is used. Refer to rasterize_meshes.py for 
+# explanations of these parameters. Refer to docs/notes/renderer.md for an explanation of 
+# the difference between naive and coarse-to-fine rasterization. 
+raster_settings = RasterizationSettings(
+    image_size=512, 
+    blur_radius=0.0, 
+    faces_per_pixel=1, 
+)
+
+# Place a point light in front of the object. As mentioned above, the front of the cow is facing the 
+# -z direction. 
+lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
+
+# Create a Phong renderer by composing a rasterizer and a shader. The textured Phong shader will 
+# interpolate the texture uv coordinates for each vertex, sample from a texture image and 
+# apply the Phong lighting model
+renderer = MeshRenderer(
+    rasterizer=MeshRasterizer(
+        cameras=cameras, 
+        raster_settings=raster_settings
+    ),
+    shader=SoftPhongShader(
+        device=device, 
+        cameras=cameras,
+        lights=lights
+    )
+)
+# ---------------------------------
+images = renderer(mesh)
+plt.figure(figsize=(10, 10))
+plt.imshow(images[0, ..., :3].cpu().numpy())
+plt.axis("off");
+# -------------------------------
+print("Done rendering")
 
 plt.grid('off')
 plt.axis('off')
 plt.gcf().set_facecolor('black')
-plt.savefig('render.png')
+plt.savefig('render1.png')
+
+print("Done plotting")
