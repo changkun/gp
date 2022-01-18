ad8272168f518b04203cbd4ea56ef34f791cb336
Marcel Quanz <marcel.quanz@gmail.com>
2022-01-18 23:39:41 +0100

    homework: add marlq's solution 06 (#70)
    * Solution for homework 6.
    
    * Commented out OpenMP problem solver line and Todo.

---
+++ homeworks/MarlQ/6-dda1/test.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022 LMU Munich Geometry Processing Authors. All rights reserved.
+# Created by Changkun Ou <https://changkun.de>.
+#
+# Use of this source code is governed by a GNU GPLv3 license that can be found
+# in the LICENSE file.
+
+import torch
+from pytorch3d.utils import ico_sphere
+from pytorch3d.io import save_obj
+
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+torch.cuda.set_device(device)
+print(device)
+
+m = ico_sphere(level=1)
+print(m.verts_packed().shape, m.faces_packed().shape)
