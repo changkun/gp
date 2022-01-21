58b27a2e4a02798b6970440e24ba005d7297a9bc
Marcel Quanz <marcel.quanz@gmail.com>
2022-01-21 15:28:43 +0100

    The soft object vertices are displaces along the average normal of all overlapping hard object vertices, providing much more sensible geometry.

--- projects/MarlQ/src/main.py
+++ projects/MarlQ/src/main.py
@@ -35,61 +35,34 @@ hardObject = set(bpy.context.selected_objects).difference(set([softObject])).pop
 
 # FIXME: Not enough objects selected
 
-print(softObject)
-
 # STEP 1: Find overlapping faces, and displace vertices so that they no longer overlap
 
-# Get their world matrix
+# Get the verts and faces in world coordinates
 mat1 = softObject.matrix_world
 mat2 = hardObject.matrix_world
 
-# Get the geometry in world coordinates
 verts1 = [mat1 @ v.co for v in softObject.data.vertices] 
 poly1 = [p.vertices for p in softObject.data.polygons]
 
 verts2 = [mat2 @ v.co for v in hardObject.data.vertices] 
 poly2 = [p.vertices for p in hardObject.data.polygons]
 
-# Create the BVH trees
-depsgraph = bpy.context.evaluated_depsgraph_get()
-bvh1 = BVHTree.FromPolygons( verts1, poly1 )
-bvh2 = BVHTree.FromPolygons( verts2, poly2 )
-
-# Overlapping vertices (list of index pairs, first belongs to the soft object, the other to the hard object)
-overlap = bvh2.overlap(bvh1)
-overlap_fi = list(dict.fromkeys([i1 for (i1, i2) in overlap]))
-print(overlap_fi)
-
 # Vertices of soft object inside hard object
 inside_verts = [i for i in range(len(verts1)) if insideMesh( localC(verts1[i], hardObject), hardObject)]
 average_displace = Vector((0.0, 0.0, 0.0))
 
-for vert1_index in inside_verts:
-    vert1_global = verts1[vert1_index]
-    vert1_local_ho = localC(vert1_global, hardObject)
-    dist_min = inf
-    closest_min = Vector((0.0, 0.0, 0.0))
-    for face2_index in overlap_fi:
-        face2 = poly2[face2_index]
-        f2_v1, f2_v2, f2_v3 = localC(verts2[face2[0]], hardObject), localC(verts2[face2[1]], hardObject), localC(verts2[face2[2]], hardObject)
-        #dist = abs(distance_point_to_plane(vert1_local_ho, f2_v1, normal(f2_v1, f2_v2, f2_v3)))
-        closest = closest_point_on_tri(vert1_local_ho, f2_v1, f2_v2, f2_v3)
-        # TODO: Check if closest is inside softMesh, otherwise dismiss
-            #TODO: Sample other points along dismissed face
-        dist = closest.length
-        if dist < dist_min:
-            dist_min = dist
-            closest_min = closest
-    average_displace = average_displace + closest_min
-    #softObject.data.vertices[vert1_index].co = localC(globalC(closest_min, hardObject), softObject)
-    #hit, hitloc, _, _ = hardObject.ray_cast(vert1_local_ho, -localC(globalC(closest_min, hardObject), softObject))
-    #if hit:
-        #softObject.data.vertices[vert1_index].co = localC(globalC(hitloc, hardObject), softObject)
+inside_vert_ho = [i for i in range(len(verts2)) if insideMesh( localC(verts2[i], softObject), softObject)]
+
+
+# Calculate the average normal of all hard object vertices that are inside the soft object
+for vert2_index in inside_vert_ho:
+    normal = hardObject.data.vertices[vert2_index].normal
+    average_displace = average_displace + normal
     
 average_displace = average_displace * (1 / len(inside_verts))
-print(average_displace)
 dist_total = 0
 
+# Use this average to displace all soft object vertices inside the hard object
 for vert1_index in inside_verts:
     vert1_global = verts1[vert1_index]
     vert1_local_ho = localC(vert1_global, hardObject)
@@ -100,16 +73,11 @@ for vert1_index in inside_verts:
         dist_total = dist_total + dist
         softObject.data.vertices[vert1_index].co = hitloc_local
 
-
 # FIXME: objects don't overlap
 # FIXME: all vertices overlap
 
-# STEP 2:
-print(dist_total)
+# STEP 2: Add the lost volume back
 outside_verts = [i for i in range(len(verts1)) if i not in inside_verts]
-
-#dist_each = dist_total / len(outside_verts)
-#print(dist_each)
 
 # Calculate shortest distance between an inside and an outside vert
 shortest_dist = inf
@@ -121,8 +89,6 @@ for vert1_index in inside_verts:
 
         if dist < shortest_dist:
             shortest_dist = dist
-
-print(shortest_dist)
 
 inverse_square_sum = 0
 for vert1_index in outside_verts:
