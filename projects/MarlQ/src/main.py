390868468302af63666830fcc13f7c9aa7065e0e
Marcel Quanz <marcel.quanz@gmail.com>
2022-01-31 17:52:01 +0100

    Added an elasticity parameter (implementation is lazy, because it displaces by an absolute, user-defined distance, not any distance defined by its surroundings). Changed the invcerse square falloff (is it better?).

--- projects/MarlQ/src/main.py
+++ projects/MarlQ/src/main.py
@@ -15,19 +15,47 @@ from mathutils.geometry import (
         normal)
 from math import pi, acos, inf
 
-
+# Determines whether point is inside mesh
 def insideMesh(point, mesh):
     _, closest, normal, _ = mesh.closest_point_on_mesh(point)
     p2 = closest-point
     v = p2.dot(normal)
     return not(v < 0.0)
 
+# Converts from global to obj local coordinates
 def localC(point, obj):
     return obj.matrix_world.inverted() @ point
 
+# Converts from obj local to global coordinates
 def globalC(point, obj):
     return obj.matrix_world @ point
 
+# Gets all connected vert islands in verts and returns them separately as indices
+def getIslands(verts):
+    islands = []
+    for vert in verts:
+        island_verts = [vert]
+        for i_vert in island_verts:
+            for edge in i_vert.link_edges:
+                v_other = edge.other_vert(i_vert)
+                if v_other in verts and v_other not in island_verts:
+                    island_verts.append(v_other)
+                    verts.remove(v_other)
+        islands.append(island_verts)
+    islands_i = []
+    for island in islands:
+        island_indices = [v.index for v in island]
+        islands_i.append(island_indices)
+    return islands_i
+                    
+        
+# Variables
+delta_initial = 3.0
+delta_increase = 0.1
+delta = delta_initial
+
+elasticity = 0.3
+elasticity_factor = -0.2
 # TODO: Multiple hard objects
 
 softObject = bpy.context.object
@@ -53,25 +81,38 @@ average_displace = Vector((0.0, 0.0, 0.0))
 
 inside_vert_ho = [i for i in range(len(verts2)) if insideMesh( localC(verts2[i], softObject), softObject)]
 
-
-# Calculate the average normal of all hard object vertices that are inside the soft object
-for vert2_index in inside_vert_ho:
-    normal = hardObject.data.vertices[vert2_index].normal
-    average_displace = average_displace + normal
-    
-average_displace = average_displace * (1 / len(inside_verts))
 dist_total = 0
 
-# Use this average to displace all soft object vertices inside the hard object
+
 for vert1_index in inside_verts:
-    vert1_global = verts1[vert1_index]
-    vert1_local_ho = localC(vert1_global, hardObject)
-    hit, hitloc, normal, index = hardObject.ray_cast(vert1_local_ho, average_displace)
-    if hit:
-        hitloc_local = localC(globalC(hitloc, hardObject), softObject)
-        dist = (hitloc_local - softObject.data.vertices[vert1_index].co).length
-        dist_total = dist_total + dist
-        softObject.data.vertices[vert1_index].co = hitloc_local
+    # Calculate the average normal of all hard object vertices that are inside the soft object
+    len_delta = 0
+    while len_delta < 1:
+        # TODO: Only look at vertices of inside_vert_ho that are actually part of an island which pierces through the island of vert1. This would eliminate a lot of issues of setting a high delta value
+        for vert2_index in inside_vert_ho:
+            if (verts2[vert2_index] - verts1[vert1_index]).length < delta:
+                normal = hardObject.data.vertices[vert2_index].normal
+                average_displace = average_displace + normal
+                len_delta = len_delta + 1
+        if len_delta < 1:
+            # Delta was not high enough
+            delta = delta + delta_increase
+    if len_delta > 0:
+        average_displace = average_displace * (1 / len_delta)
+        
+        # Use this average to displace all soft object vertices inside the hard object
+        vert1_global = verts1[vert1_index]
+        vert1_local_ho = localC(vert1_global, hardObject)
+        hit, hitloc, normal, index = hardObject.ray_cast(vert1_local_ho, average_displace)
+        if hit:
+            hitloc_local = localC(globalC(hitloc, hardObject), softObject)
+            dist = (hitloc_local - softObject.data.vertices[vert1_index].co).length
+            dist_total = dist_total + dist
+            softObject.data.vertices[vert1_index].co = hitloc_local
+    else:
+        print("Delta was too small")
+    delta = delta_initial
+        
 
 # FIXME: objects don't overlap
 # FIXME: all vertices overlap
@@ -99,11 +140,22 @@ for vert1_index in outside_verts:
         dist = (vert1 - vert2).length
         if dist < dist_min:
             dist_min = dist # TODO: Save these for later
-    inverse_square_sum += 1 / ( shortest_dist + dist_min * dist_min )
+    if dist_min < elasticity:
+        print(dist_min)
+        move_dist = elasticity_factor * 1 / (1 + (dist_min-shortest_dist) * (dist_min-shortest_dist))
+        normal = softObject.data.vertices[vert1_index].normal
+        newPos = softObject.data.vertices[vert1_index].co + move_dist * normal
+        softObject.data.vertices[vert1_index].co = newPos
+        dist_total = dist_total - move_dist
+        print(move_dist)
+        #outside_verts.remove(vert1_index)
+        
+    else:
+        inverse_square_sum += 1 / ( 1 + (dist_min-shortest_dist) * (dist_min-shortest_dist) )
     
 x_fac = dist_total / inverse_square_sum # The factor by which the inverse square of each distance is multiplied so that the total is always dist_total
-print(x_fac)
 
+test = 0
 for vert1_index in outside_verts:
     vert1_global = verts1[vert1_index]
     vert1 = localC(verts1[vert1_index], softObject)
@@ -114,7 +166,8 @@ for vert1_index in outside_verts:
         if dist < dist_min:
             dist_min = dist # TODO: Save these for later
     #print(dist_min)
-    move_dist = x_fac * 1 / (shortest_dist + dist_min * dist_min)
+    move_dist = x_fac * 1 / (1 + (dist_min-shortest_dist) * (dist_min-shortest_dist))
+    test += move_dist
     #print(move_dist)
     normal = softObject.data.vertices[vert1_index].normal
     newPos = softObject.data.vertices[vert1_index].co + move_dist * normal
